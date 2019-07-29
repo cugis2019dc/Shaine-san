4b0657d66fb03e38b11e8e335288abff9ae8f3c6
Shaine-San <53440579+Shaine-San@users.noreply.github.com>
2019-07-29 12:51:51 -0400

    Add files via upload

---
+++ day 1.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+Hello my name is shiane
+
+print("my name is shaine")
+
+print("i want food")
+
+5/2
+
+5*2
+
+5-2
+
+5^2
+
+8/9*2
+
+print("5*2") 
